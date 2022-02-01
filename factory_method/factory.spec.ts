57071829bfca2ff58453c14b60a53dfb3f466a69
starfrogsplash <ali_khalil@hotmail.co.uk>
2022-02-01 21:11:00 +0000

    setting up debugger

--- factory_method/factory.spec.ts
+++ factory_method/factory.spec.ts
@@ -1,9 +1,15 @@
 import {ConcreteCreator1, ConcreteCreator2, clientCode} from './factory'
 
 it('returns ConcreteCreator1', () => {
-
     console.log('App: Launched with the ConcreteCreator1.');
     const result = clientCode(new ConcreteCreator1());
+    console.log(result)
+    expect(result).toEqual("Creator: The same creator's code has just worked with {Result of the ConcreteProduct1}")
+})
 
-    expect(result).toEqual('')
+
+xit('returns ConcreteCreator2', () => {
+    console.log('App: Launched with the ConcreteCreator2.');
+    const result = clientCode(new ConcreteCreator2());
+    expect(result).toEqual("Creator: The same creator's code has just worked with {Result of the ConcreteProduct2}")
 })
--- factory_method/factory.ts
+++ factory_method/factory.ts
@@ -75,23 +75,21 @@ class ConcreteProduct2 implements Product {
  * its base interface. As long as the client keeps working with the creator via
  * the base interface, you can pass it any creator's subclass.
  */
-function clientCode(creator: Creator) {
-    // ...
+const clientCode = (creator: Creator) => {
     console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
-    console.log(creator.someOperation());
-    // ...
+    return creator.someOperation()
 }
 
 /**
  * The Application picks a creator's type depending on the configuration or
  * environment.
  */
-console.log('App: Launched with the ConcreteCreator1.');
-clientCode(new ConcreteCreator1());
-console.log('');
+// console.log('App: Launched with the ConcreteCreator1.');
+// clientCode(new ConcreteCreator1());
+// console.log('');
 
-console.log('App: Launched with the ConcreteCreator2.');
-clientCode(new ConcreteCreator2());
+// console.log('App: Launched with the ConcreteCreator2.');
+// clientCode(new ConcreteCreator2());
 
 
 export {
